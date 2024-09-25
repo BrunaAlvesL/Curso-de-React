ac613ebcff2421f531b21bb86a08b9cdbe518f0d
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2024-09-25 06:50:46 -0300

    subi

---
+++ index.jsx
@@ -0,0 +1,13 @@
+const element = <h1>Olá mundo!</h1>
+
+const anotherElement = (
+    <div>
+        <h1>Texte</h1>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+)
