aeb48882945944b3b6790b065163f6387f4139ae
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2024-09-25 08:09:49 -0300

    subi

---
+++ Exercicio-01/src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return(
+    <><><div>
+      <img src="/react.png" alt="" />
+    </div><h1>React</h1><p>A biblioteca para interfaces de usuário web e nativas.</p><button>Aprenda React</button><button>Referência da API</button><hr /></><div><h1>Crie interfaces de usuários de componentes</h1><p>React permite que você construa interfaces de usuário a partir de pedaços individuais chamados componentes.</p></div><hr /><div><h1>Escreva componentes com código e marcação</h1><p>Componentes React são funções JavaScript. A sintaxe de marcação é chamada de JSX. É uma extensão de sintaxe JavaScript popularizada pelo React.</p></div><hr /><div>
+        <h1>Primeiros passos</h1>
+        <ul>
+          <li>Uso de dados dinamicos no JSX</li>
+          <li>Criação de novos componentes</li>
+          <li>Estilização de componentes</li>
+          <li>Reutilização de componentes</li>
+          <li>Uso de props e children</li>
+          <li>Uso de eventos do JavaScript</li>
+        </ul>
+      </div></>
+
+  )
+}
+  
