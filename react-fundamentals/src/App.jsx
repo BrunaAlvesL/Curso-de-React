f069e4385f631f1ae5a2419dcac1d8402fbe2697
Bruna <79263157+BrunaAlvesL@users.noreply.github.com>
2024-09-27 07:43:25 -0300

    subi

---
+++ react-fundamentals/src/App.jsx
@@ -0,0 +1,21 @@
+import StatusText from "./components/StatusText"
+import Subtitle from "./components/Subtitle"
+import Title from "./components/Title"
+
+export default function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#2c2c2d",
+        display: "grid",
+        minHeight: "100vh",
+        placeContent: "center",
+        textAlign: "center"
+      }}
+    >
+      <Title />
+      <Subtitle />
+      <StatusText />
+    </div>
+  )
+}
